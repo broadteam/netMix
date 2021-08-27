820a2abc44e0dd6ab96a5b8cf1bf3955b99706b9
yangteng <yangteng@yangtengs-macOS.local>
2021-08-27 23:41:14 +0800

    mysql通道版支持加入

--- services/netmix/systemd/etc/services.d/startup.sh
+++ services/netmix/systemd/etc/services.d/startup.sh
@@ -10,6 +10,18 @@ mknod /dev/net/tun c 10 200
 chmod 600 /dev/net/tun
 chmod -R 777 /etc/openvpn
 
+# 启动php-fpm
+if [ -f '/usr/sbin/php-fpm7' ]; then
+    /usr/sbin/php-fpm7
+fi
+
+# 启动nginx
+if [ -f '/usr/sbin/nginx' ]; then
+    mkdir -p /run/nginx
+    chmod -R 777 /run/nginx
+    /usr/sbin/nginx
+fi
+
 # 有openvpn证书时连接vpn
 if [ -f $OPENVPN_CERT_CONF ]; then
   /usr/sbin/openvpn --user nobody --group nobody --daemon --config $OPENVPN_CERT_CONF
